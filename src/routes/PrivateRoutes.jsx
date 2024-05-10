d30c08f8dc61d797b2ec2b4334330bbd217cf140
Hm-masum <hhmasum88@gmail.com>
2024-05-10 15:21:05 +0600

    private route commit

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,7 +1,19 @@
-const PrivateRoutes = () => {
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../hook/useAuth";
+
+const PrivateRoutes = ({children}) => {
+    const {user,loading}=useAuth()
+    const location=useLocation()
+
+    if(loading){
+        return <div className="flex justify-center h-[50vh]"><span className="loading loading-bars loading-lg"></span></div>
+    }
+    if(user){
+        return children;
+    }
     return (
         <div>
-            
+            <Navigate to={'/login'} state={location.pathname} replace={true}></Navigate>
         </div>
     );
 };
